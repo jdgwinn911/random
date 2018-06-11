52adb122e4b62b57dacdde15c2e7d56d47d40415
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-11 13:45:06 -0400

    heredoc

---
+++ heredoc.rb
@@ -0,0 +1,16 @@
+puts "Let's practice everything."
+puts 'you would need to know \ bout escapes with \\ that do \n new lines and \t tabs'
+
+#the << end is a "heredoc"
+poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love 
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+puts "&&&&&&&&&&&&&&&"
+puts poem
+puts"&&&&&&&&&&&&&&&&"
